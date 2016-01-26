c1f07a8a072aa41fef25126136b7044fb4569b3c
Apple <tiffine.koch@gmail.com>
2016-01-26 08:40:12 -0800

    updated files

--- public/js/main.js
+++ public/js/main.js
@@ -27,5 +27,6 @@ var newContact = ({name: name, email: email, location: loc, phone: phone, face: 
 }
 
 function findName(event) {
-  console.log(event.target.closest('tr').indexOf());
+  // console.log(event.target.closest('tr').indexOf());
+  console.log($(this).closest('tr').index());
 }
